6a690c10fabecfd76b6545fc06608aa4fae6bb33
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-07-26 23:46:44 +0000

    Added file task 14

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+module np_matmul
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    performs matrix multiplication
+    Args:
+        mat1 (int): ndarray to multiply
+        mat2 (int): ndarray to multiply
+    """
+    return np.matmul(mat1, mat2)
