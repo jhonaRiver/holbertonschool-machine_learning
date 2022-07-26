49679b6354c5eb655342ea962612c6d5a27529f7
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-07-26 22:49:49 +0000

    Added file task 11

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+module np_transpose
+"""
+
+
+def np_transpose(matrix):
+    """
+    transposes matrix
+    Args:
+        matrix (int): ndarray to be transposed
+    """
+    return matrix.transpose()
