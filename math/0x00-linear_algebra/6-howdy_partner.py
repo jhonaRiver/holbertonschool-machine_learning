cd70cb9d5fd5657c7e77f46d0381350e08ae41da
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-07-26 16:00:09 +0000

    Added file task 6

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+module cat_arrays
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    concatenates two arrays
+    Args:
+        arr1 (int/float): list of ints/floats
+        arr2 (int/float): list of ints/floats
+    """
+    return arr1 + arr2
