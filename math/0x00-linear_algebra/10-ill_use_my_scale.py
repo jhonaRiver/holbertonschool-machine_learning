734d366b545c48b218e484abaec917186e22149b
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-07-26 22:33:43 +0000

    Added file task 10

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+module np_shape
+"""
+import numpy as np
+
+
+def np_shape(matrix):
+    """
+    calculates the shape of a numpy.ndarray
+    Args:
+        matrix (int): ndarray to calculate the shape
+    """
+    return matrix.shape
