76ada3aaf4f4dd9bb9438a6e65fe0f9be0de15f1
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-08-02 01:50:43 +0000

    Modified file task 4

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -10,4 +10,7 @@ plt.hist(student_grades, bins=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
 plt.xlabel('Grades')
 plt.ylabel('Number of Students')
 plt.title('Project A')
+plt.xlim(0, 100)
+plt.xticks(np.arange(0, 101, 10))
+plt.ylim(0,30)
 plt.show()
