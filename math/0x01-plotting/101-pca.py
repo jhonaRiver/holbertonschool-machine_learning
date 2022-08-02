3b17aba16845f361522f07f10fe2705198a7c1b3
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-08-02 02:47:31 +0000

    Modified file task 8

--- math/0x01-plotting/101-pca.py
+++ math/0x01-plotting/101-pca.py
@@ -12,12 +12,13 @@ norm_data = data - data_means
 _, _, Vh = np.linalg.svd(norm_data)
 pca_data = np.matmul(norm_data, Vh[:3].T)
 
-plt.subplot(1, 1, 1, projection='3d')
-plt.scatter(pca_data[:, 0], pca_data[:, 1], pca_data[:, 2],
+figure = plt.figure()
+axis= figure.add_subplot(1, 1, 1, projection='3d')
+axis.scatter(pca_data[:, 0], pca_data[:, 1], pca_data[:, 2],
             c=labels, cmap=plt.get_cmap('plasma'),
             label=labels)
-plt.xlabel('U1')
-plt.ylabel('U2')
-plt.zlabel('U3')
+axis.set_xlabel('U1')
+axis.set_ylabel('U2')
+axis.set_zlabel('U3')
 plt.title('PCA of Iris Dataset')
 plt.show()
