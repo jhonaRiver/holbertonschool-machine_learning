ccf71fd303977b9f1a27f625fd54a979e262492d
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-08-03 01:54:29 +0000

    Modified file task 9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -5,6 +5,8 @@ module summation_i_squared
 
 
 def summation_i_squared(n):
+    if type(n) != int:
+        return None
     if n == 1:
         return n
     return ((n ** 2) + summation_i_squared(n - 1))
