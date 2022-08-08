e6e112e493cc57808b45def8d22230d1c304c2da
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-08-08 17:50:37 +0000

    Added file task 0

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+module Poisson
+"""
+
+
+class Poisson:
+    """
+    class that represents a poisson distribution
+    """
+    def __init__(self, data=None, lambtha=1.):
+        """
+        class constructor
+        Args:
+            data (list, optional): list of the data to be used to estimate
+                                   the distribution. Defaults to None.
+            lambtha (float, optional): expected number of occurences in a
+                                       given time frame. Defaults to 1..
+        """
+        self.data = data
+        self.lambtha = lambtha
+
+    @property
+    def lambtha(self):
+        """
+        getter function
+        Returns:
+            float: expected number of occurences in a given time frame
+        """
+        return self.__lambtha
+
+    @lambtha.setter
+    def lambtha(self, lambtha):
+        """
+        setter function
+        Args:
+            lambtha (float): expected number of occurences in a given time
+                             frame
+
+        Raises:
+            ValueError: lambtha must be a positive value
+            TypeError: data must be a list
+            ValueError: data must contain multiple values
+        """
+        if lambtha <= 0:
+            raise ValueError("lambtha must be a positive value")
+        if self.data is None:
+            self.__lambtha = float(lambtha)
+        else:
+            if type(self.data) != list:
+                raise TypeError("data must be a list")
+            if len(self.data) < 2:
+                raise ValueError("data must contain multiple values")
+            sum = 0
+            for value in self.data:
+                sum += self.data[value]
+            lambtha = sum / len(self.data)
+            self.__lambtha = float(lambtha)
