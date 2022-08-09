dd4dc7038e18d3a8e757b3854c03e46db38e4cad
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-08-09 18:11:36 +0000

    Added file task 6

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+"""
+module Normal class
+"""
+
+
+class Normal:
+    """
+    class that represents a normal distribution
+    """
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """
+        class constructor
+        Args:
+            data (list, optional): list of data to be used to estimate the
+                                   distribution. Defaults to None.
+            mean (float, optional): mean of the distribution. Defaults to 0..
+            stddev (float, optional): standard deviation of the distribution.
+                                       Defaults to 1..
+        """
+        self.data = data
+        self.mean = mean
+        self.stddev = stddev
+
+    @property
+    def mean(self):
+        """
+        getter function
+        Returns:
+            float: mean of the distribution
+        """
+        return self.__mean
+
+    @mean.setter
+    def mean(self, mean):
+        """
+        setter function
+        Args:
+            mean (float): mean of the distribution
+
+        Raises:
+            TypeError: data must be a list
+            ValueError: data must contain multiple values
+        """
+        if self.data is None:
+            self.__mean = float(mean)
+        else:
+            if type(self.data) != list:
+                raise TypeError("data must be a list")
+            if len(self.data) < 2:
+                raise ValueError("data must contain multiple values")
+            sum = 0
+            for i in range(0, len(self.data)):
+                sum += self.data[i]
+            mean = sum / len(self.data)
+        self.__mean = float(mean)
+
+    @property
+    def stddev(self):
+        """
+        getter function
+        Returns:
+            float: standard deviation of the distribution
+        """
+        return self.__stddev
+
+    @stddev.setter
+    def stddev(self, stddev):
+        """
+        setter function
+        Args:
+            stddev (float): standard deviation of the distribution
+
+        Raises:
+            ValueError: stddev must be a positive value
+            TypeError: data must be a list
+            ValueError: data must contain multiple values
+        """
+        if stddev <= 0:
+            raise ValueError("stddev must be a positive value")
+        if self.data is None:
+            self.__stddev = float(stddev)
+        else:
+            if type(self.data) != list:
+                raise TypeError("data must be a list")
+            if len(self.data) < 2:
+                raise ValueError("data must contain multiple values")
+            sum = 0
+            for i in range(0, len(self.data)):
+                sum += ((self.data[i] - self.__mean) ** 2)
+            var = sum / len(self.data)
+            stddev = var ** 0.5
+        self.__stddev = float(stddev)
