b5095247326fac3acf91ead5fd9d74fbd16fda0a
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-08-10 00:13:38 +0000

    Added pmf function task 11

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -93,3 +93,28 @@ class Binomial:
             mean = sum(self.data) / len(self.data)
             p = mean / self.__n
         self.__p = float(p)
+
+    def pmf(self, k):
+        """
+        calculates the value of the PMF for a given number of "successes"
+        Args:
+            k (int): number of "successes"
+
+        Returns:
+            float: PMF value for k
+        """
+        k = int(k)
+        factor_k = 1
+        factor_n = 1
+        factor_c = 1
+        if k < 0:
+            return 0
+        for i in range(1, k+1):
+            factor_k *= i
+        for i in range(1, self.__n+1):
+            factor_n *= i
+        for i in range(1, (self.__n - k)+1):
+            factor_c *= i
+        comb = factor_n / (factor_c * factor_k)
+        prob = (self.__p ** k) * ((1 - self.__p) ** (self.__n - k))
+        return (comb * prob)
