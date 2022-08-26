0421a13642abe014f18d10c1e4a17cf339c59aa6
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-08-26 15:35:34 +0000

    Added file task 1

---
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+module create_layer
+"""
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+
+def create_layer(prev, n, activation):
+    """
+    function that creates layer
+    Args:
+        prev (tensor): tensor output of the previous layer
+        n (int): number of nodes in the layer to create
+        activation (function): activation function that the layer should use
+
+    Returns:
+        tensor: tensor output of the layer
+    """
+    weight = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    layer = tf.keras.layers.Dense(units=n, activation=activation,
+                                  kernel_initializer=weight, name='layer')
+    return layer(prev)
