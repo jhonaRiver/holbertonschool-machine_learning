8f5b1c9235f872f9475042b37d201e048069a5ba
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-08-26 18:04:42 +0000

    Added file task 7

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+module evaluate
+"""
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+
+def evaluate(X, Y, save_path):
+    """
+    evaluates the output of a neural network
+    Args:
+        X (ndarray): contains the input data to evaluate
+        Y (ndarray): contains the one-hot labels for X
+        save_path (str): location to load the model from
+
+    Returns:
+        float: network's prediction, accuracy and loss
+    """
+    with tf.Session() as sess:
+        save_NN = tf.train.import_meta_graph("{}.meta".format(save_path))
+        save_NN.restore(sess, save_path)
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
+        prediction = sess.run(y_pred, feed_dict={x: X, y: Y})
+        accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
+        loss = sess.run(loss, feed_dict={x: X, y: Y})
+        return prediction, accuracy, loss
