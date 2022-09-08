f4cdedd199f3a8c80b9bbf6ce4292fcaa94d196f
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-09-08 22:03:12 +0000

    Modified file task 3

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -19,8 +19,8 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     """
     initializer = tf.keras.initializers.VarianceScaling(scale=2.0,
                                                         mode=("fan_avg"))
-    kernel_regularizer = tf.keras.regularizers.L2(lambtha)
-    layer = tf.keras.layers.Dense(n, activation=activation,
-                                  kernel_initializer=initializer, name='layer',
-                                  kernel_regularizer=kernel_regularizer)
+    regularizer = tf.keras.regularizers.L2(lambtha)
+    layer = tf.keras.layers.Dense(units=n, activation=activation,
+                                  kernel_initializer=initializer,
+                                  kernel_regularizer=regularizer)
     return layer(prev)
