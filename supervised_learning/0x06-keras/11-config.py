f680133e23ad00b3832a48df07a621f00510cc0b
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-09-14 01:56:24 +0000

    Added file task 11

---
+++ supervised_learning/0x06-keras/11-config.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+module save_config and load_config
+"""
+
+
+def save_config(network, filename):
+    """
+    saves a model's configuration in JSON format
+    Args:
+        network (keras): model whose configuration should be saved
+        filename (str): path of the file that the configuration should be
+                        saved to
+    Returns:
+        None
+    """
+    with open(filename, 'w') as f:
+        f.write(network.to_json())
+    f.close()
+    return None
+
+
+def load_config(filename):
+    """
+    loads a model with a specific configuration
+    Args:
+        filename (str): path of the file containing the model's configuration
+                        in JSON format
+    Returns:
+        loaded model
+    """
+    with open(filename, 'r') as f:
+        model = K.models.model_from_json(f.read())
+    f.close()
+    return model
