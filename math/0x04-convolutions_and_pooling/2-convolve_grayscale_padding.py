1d1db8ac3014e8d789682e045cf52dd8203a8d16
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-09-15 15:58:58 +0000

    Added file task 2

--- math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -2,6 +2,7 @@
 """
 module convolve_grayscale_padding
 """
+import numpy as np
 
 
 def convolve_grayscale_padding(images, kernel, padding):
@@ -14,3 +15,19 @@ def convolve_grayscale_padding(images, kernel, padding):
     Returns:
         ndarray containing the convolved images
     """
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    ph, pw = padding[0], padding[1]
+    oh = h + 2 * ph - kh + 1
+    ow = w + 2 * pw - kw + 1
+    dim = (m, oh, ow)
+    out = np.zeros(dim)
+    padded = np.pad(images, pad_width=((0, 0), (ph, ph), (pw, pw)),
+                    mode='constant', constant_values=0)
+    for i in range(dim[1]):
+        for j in range(dim[2]):
+            x = i + kh
+            y = j + kw
+            M = padded[:, i:x, j:y]
+            out[:, i, j] = np.tensordot(M, kernel)
+    return out
