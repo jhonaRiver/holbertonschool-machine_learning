c2153b4341cec954e74c2071b02118c13f076f69
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-09-15 19:03:14 +0000

    Added file task 6

--- math/0x04-convolutions_and_pooling/6-pool.py
+++ math/0x04-convolutions_and_pooling/6-pool.py
@@ -2,6 +2,7 @@
 """
 module pool
 """
+import numpy as np
 
 
 def pool(images, kernel_shape, stride, mode='max'):
@@ -15,3 +16,20 @@ def pool(images, kernel_shape, stride, mode='max'):
     Returns:
         ndarray containing the pooled images
     """
+    m, h, w, c = images.shape
+    kh, kw = kernel_shape[0], kernel_shape[1]
+    sh, sw = stride[0], stride[1]
+    oh = int((h - kh) / sh) + 1
+    ow = int((w - kw) / sw) + 1
+    dim = (m, oh, ow, c)
+    out = np.zeros(dim)
+    for i in range(dim[1]):
+        for j in range(dim[2]):
+            x = (i * sh) + kh
+            y = (j * sw) + kw
+            M = images[:, (i * sh):x, (j * sw):y, :]
+            if mode == 'max':
+                out[:, i, j, :] = np.max(M, axis=(1, 2))
+            else:
+                out[:, i, j, :] = np.average(M, axis=(1, 2))
+    return out
