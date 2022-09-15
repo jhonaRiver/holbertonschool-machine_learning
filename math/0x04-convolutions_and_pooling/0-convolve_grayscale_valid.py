8603edaf5c3f2fb1bfaab94c7296e1e872e33c00
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-09-15 14:56:35 +0000

    Added file task 0

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -2,6 +2,7 @@
 """
 module convolve_grayscale_valid
 """
+import numpy as np
 
 
 def convolve_grayscale_valid(images, kernel):
@@ -13,3 +14,14 @@ def convolve_grayscale_valid(images, kernel):
     Returns:
         ndarray containing the convolved images
     """
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    dim = (m, h - kh + 1, w - kw + 1)
+    out = np.zeros(dim)
+    for i in range(dim[1]):
+        for j in range(dim[2]):
+            x = i + kh
+            y = j + kw
+            M = images[:, i:x, j:y]
+            out[:, i, j] = np.tensordot(M, kernel)
+    return out
