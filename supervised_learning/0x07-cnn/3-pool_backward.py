510d8a3796d0c1f772969a71fa395bad5a2b5117
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-09-21 17:31:13 +0000

    Added file task 3

--- supervised_learning/0x07-cnn/3-pool_backward.py
+++ supervised_learning/0x07-cnn/3-pool_backward.py
@@ -20,3 +20,29 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
     Returns:
         partial derivatives with respect to the previous layer
     """
+    (m, h_prev, w_prev, c_prev) = A_prev.shape
+    (m, h_new, w_new, c_new) = dA.shape
+    (kh, kw) = kernel_shape
+    sh, sw = stride
+    dA_prev = np.zeros(A_prev.shape)
+    for i in range(m):
+        a_prev = A_prev[i]
+        for h in range(h_new):
+            for w in range(w_new):
+                for c in range(c_new):
+                    v_start = h * sh
+                    v_end = v_start + kh
+                    h_start = w * sw
+                    h_end = h_start + kw
+                    if mode == 'max':
+                        a_slice = a_prev[v_start:v_end, h_start:h_end, c]
+                        mask = (a_slice == np.max(a_slice))
+                        dA_prev[i, v_start:v_end, h_start:h_end, c] +=\
+                            np.multiply(mask, dA[i, h, w, c])
+                    else:
+                        da = dA[i, h, w, c]
+                        shape = kernel_shape
+                        avg = da / (kh * kw)
+                        Z = np.ones(shape) * avg
+                        dA_prev[i, v_start:v_end, h_start:h_end, c] += Z
+    return dA_prev
