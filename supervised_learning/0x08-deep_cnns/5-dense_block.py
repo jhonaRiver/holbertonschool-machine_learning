c5041e825944737583cd9c9676941cb40ec246e2
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-09-28 10:15:01 -0400

    Added file task 5

--- supervised_learning/0x08-deep_cnns/5-dense_block.py
+++ supervised_learning/0x08-deep_cnns/5-dense_block.py
@@ -18,3 +18,18 @@ def dense_block(X, nb_filters, growth_rate, layers):
         concatenated output of each layer within the Dense Block and the
         number of filters within the concatenated outputs
     """
+    init = K.initializers.he_normal()
+    for i in range(layers):
+        batch1 = K.layers.BatchNormalization()(X)
+        relu1 = K.layers.Activation('relu')(batch1)
+        bottleneck = K.layers.Conv2D(filters=4*growth_rate, kernel_size=1,
+                                     padding='same',
+                                     kernel_initializer=init)(relu1)
+        batch2 = K.layers.BatchNormalization()(bottleneck)
+        relu2 = K.layers.Activation('relu')(batch2)
+        X_conv = K.layers.Conv2D(filters=growth_rate, kernel_size=3,
+                                 padding='same',
+                                 kernel_initializer=init)(relu2)
+        X = K.layers.concatenate([X, X_conv])
+        nb_filters += growth_rate
+    return X, nb_filters
