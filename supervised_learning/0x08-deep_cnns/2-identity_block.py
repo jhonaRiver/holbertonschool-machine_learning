e3b966d3e5315e5be875f395c1c98f517893f45f
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-09-28 10:14:29 -0400

    Added file task 2

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -15,3 +15,20 @@ def identity_block(A_prev, filters):
     Returns:
         activated output of the identity block
     """
+    init = K.initializers.he_normal()
+    activation = 'relu'
+    F11, F3, F12 = filters
+    conv1 = K.layers.Conv2D(filters=F11, kernel_size=1, padding='same',
+                            kernel_initializer=init)(A_prev)
+    batch1 = K.layers.BatchNormalization(axis=3)(conv1)
+    relu1 = K.layers.Activation('relu')(batch1)
+    conv2 = K.layers.Conv2D(filters=F3, kernel_size=3, padding='same',
+                            kernel_initializer=init)(relu1)
+    batch2 = K.layers.BatchNormalization(axis=3)(conv2)
+    relu2 = K.layers.Activation('relu')(batch2)
+    conv3 = K.layers.Conv2D(filters=F12, kernel_size=1, padding='same',
+                            kernel_initializer=init)(relu2)
+    batch3 = K.layers.BatchNormalization(axis=3)(conv3)
+    add = K.layers.Add()([batch3, A_prev])
+    final_relu = K.layers.Activation('relu')(add)
+    return final_relu
