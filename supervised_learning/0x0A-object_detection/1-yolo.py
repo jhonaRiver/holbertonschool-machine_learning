19da38cebb31b1f5a3955049e6d54130cedd346c
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-10-14 17:00:07 -0400

    Added file task 1

---
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""module class Yolo."""
+import tensorflow.keras as K
+import numpy as np
+
+
+class Yolo:
+    """class Yolo."""
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """
+        Class constructor.
+
+        Args:
+            model_path (str): path to where a Darknet Keras model is stored
+            classes_path (str): path to where the list of class names used for
+                                the Darknet model, listed in order of index,
+                                can be found
+            class_t (float): represents the box score threshold for the
+                             initial filtering step
+            nms_t (float): representing the IOU threshold for non-max
+                           suppression
+            anchors (ndarray): contains all of the anchor boxes
+        """
+        self.model = K.models.load_model(model_path)
+        with open(classes_path, 'r') as f:
+            self.class_names = [line.strip() for line in f]
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+
+    def sigmoid(self, x):
+        """Sigmoid function."""
+        return 1 / (1 + np.exp(-x))
+
+    def process_outputs(self, outputs, image_size):
+        """
+        Process the outputs of the class.
+
+        Args:
+            outputs (ndarray): contains the predictions from the Darknet model
+                               for a single image
+            image_size (ndarray): contains the image's original size
+        Returns:
+            tuple of (boxes, box_confidences, box_class_probs)
+        """
+        boxes = [pred[:, :, :, 0:4] for pred in outputs]
+        for ipred, pred in enumerate(boxes):
+            for grid_h in range(pred.shape[0]):
+                for grid_w in range(pred.shape[1]):
+                    bx = ((self.sigmoid(pred[grid_h, grid_w, :, 0]) + grid_w)
+                          / pred.shape[1])
+                    by = ((self.sigmoid(pred[grid_h, grid_w, :, 1]) + grid_h)
+                          / pred.shape[0])
+                    anchor_tensor = self.anchors[ipred].astype(float)
+                    anchor_tensor[:, 0] *= np.exp(pred[grid_h, grid_w, :, 2])\
+                        / self.model.input.shape[1]
+                    anchor_tensor[:, 1] *= np.exp(pred[grid_h, grid_w, :, 3])\
+                        / self.model.input.shape[2]
+                    pred[grid_h, grid_w, :, 0] = (bx - (anchor_tensor[:, 0] /
+                                                  2)) * image_size[1]
+                    pred[grid_h, grid_w, :, 1] = (by - (anchor_tensor[:, 1] /
+                                                  2)) * image_size[0]
+                    pred[grid_h, grid_w, :, 2] = (bx + (anchor_tensor[:, 0] /
+                                                  2)) * image_size[1]
+                    pred[grid_h, grid_w, :, 3] = (by + (anchor_tensor[:, 1] /
+                                                  2)) * image_size[0]
+        box_confidences = [self.sigmoid(pred[:, :, :, 4:5]) for pred in
+                           outputs]
+        box_class_probs = [self.sigmoid(pred[:, :, :, 5:]) for pred in outputs]
+        return boxes, box_confidences, box_class_probs
