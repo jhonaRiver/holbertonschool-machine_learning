08c920572237b82be0be6e03816c64d0e9cfa26a
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-11-07 14:01:49 -0400

    Added file task 0

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -12,3 +12,12 @@ def mean_cov(X):
     Returns:
         mean, cov
     """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    if X.shape[0] < 2:
+        raise ValueError("X must contain multiple data points")
+    n, d = X.shape
+    mean = np.mean(X, axis=0).reshape(1, d)
+    X_mean = X - mean
+    cov = np.matmul(X_mean.T, X_mean) / (n - 1)
+    return mean, cov
