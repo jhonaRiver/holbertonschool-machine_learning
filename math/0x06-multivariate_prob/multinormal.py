29cca6e10a23526f3519f506f85998b7efe3c2ea
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-11-07 14:51:31 -0400

    Added file task 2

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -13,6 +13,16 @@ class MultiNormal:
         Args:
             data (ndarray): contains the data set
         """
+        if type(data) is not np.ndarray:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        if len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        if data.shape[1] < 2:
+            raise ValueError("data must contain multiple data points")
+        d, n = data.shape
+        self.mean = np.mean(data, axis=1).reshape(d, 1)
+        X_mean = data - self.mean
+        self.cov = np.matmul(X_mean, X_mean.T) / (n - 1)
 
     def pdf(self, x):
         """
