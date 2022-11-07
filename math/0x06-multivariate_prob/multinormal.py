0ae1cb97bdc694ca9652ccedc966fe64a83a9854
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-11-07 15:08:31 -0400

    Added function task 3

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -33,3 +33,24 @@ class MultiNormal:
         Returns:
             value of the PDF
         """
+        if type(x) is not np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
+        if len(x.shape) != 2:
+            raise ValueError("x must have the shape ({}, 1)".format(
+                self.cov.shape[0]))
+        if x.shape[1] != 1:
+            raise ValueError("x must have the shape ({}, 1)".format(
+                self.cov.shape[0]))
+        if x.shape[0] != self.cov.shape[0]:
+            raise ValueError("x must have the shape ({}, 1)".format(
+                self.cov.shape[0]))
+        X_mean = x - self.mean
+        d = self.cov.shape[0]
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+        p_a = 1 / np.sqrt(((2 * np.pi) ** d) * det)
+        p_b1 = np.dot(-(X_mean).T, inv)
+        p_b2 = np.dot(p_b1, X_mean / 2)
+        p_c = np.exp(p_b2)
+        pdf = float(p_a * p_c)
+        return pdf
