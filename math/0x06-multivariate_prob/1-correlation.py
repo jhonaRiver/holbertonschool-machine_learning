da6506a4881c18e73d8c9938c83fca57c9e4079a
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-11-07 14:23:40 -0400

    Added file task 1

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -12,3 +12,15 @@ def correlation(C):
     Returns:
         correlation matrix
     """
+    if type(C) is not np.ndarray:
+        raise TypeError("C must be a numpy.ndarray")
+    if len(C.shape) != 2:
+        raise ValueError("C must be a 2D square matrix")
+    if C.shape[0] != C.shape[1]:
+        raise ValueError("C must be a 2D square matrix")
+    cov = np.diag(C)
+    cov_mat = np.expand_dims(cov, axis=0)
+    std_x = np.sqrt(cov_mat)
+    std_product = np.dot(std_x.T, std_x)
+    corr = C / std_product
+    return corr
