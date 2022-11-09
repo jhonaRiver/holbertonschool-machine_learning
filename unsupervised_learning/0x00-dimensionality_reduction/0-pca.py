91f8341318c1779b5672428c0a227491e546fa3d
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-11-09 11:48:04 -0400

    Added file task 0

--- unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -13,3 +13,9 @@ def pca(X, var=0.95):
     Returns:
         weights matrix
     """
+    u, Sigma, vh = np.linalg.svd(X, full_matrices=False)
+    cumulative_var = np.cumsum(Sigma) / np.sum(Sigma)
+    r = (np.argwhere(cumulative_var >= var))[0, 0]
+    w = vh.T
+    wr = w[:, :r + 1]
+    return wr
