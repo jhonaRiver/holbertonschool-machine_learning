7e963b4aaed8719c1055eee6507e962eca3c6e81
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-11-15 11:20:42 -0400

    Added file task 1

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -15,3 +15,29 @@ def kmeans(X, k, iterations=1000):
     Returns:
         C, clss or None, None
     """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if not isinstance(k, int) or k <= 0:
+        return None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None
+    n, d = X.shape
+    X_min = X.min(axis=0)
+    X_max = X.max(axis=0)
+    C = np.random.uniform(X_min, X_max, size=(k, d))
+    for i in range(iterations):
+        centroids = np.copy(C)
+        centroids_extended = C[:, np.newaxis]
+        distances = np.sqrt(((X - centroids_extended) ** 2).sum(axis=2))
+        clss = np.argmin(distances, axis=0)
+        for c in range(k):
+            if X[clss == c].size == 0:
+                C[c] = np.random.uniform(X_min, X_max, size=(1, d))
+            else:
+                C[c] = X[clss == c].mean(axis=0)
+        centroids_extended = C[:, np.newaxis]
+        distances = np.sqrt(((X - centroids_extended) ** 2).sum(axis=2))
+        clss = np.argmin(distances, axis=0)
+        if (centroids == C).all():
+            break
+    return C, clss
