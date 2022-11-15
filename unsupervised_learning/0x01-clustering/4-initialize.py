53e71450ffa28c9842e5dbfd0aefca89112b2a1e
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-11-15 19:34:28 -0400

    Added file task 4

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -14,3 +14,12 @@ def initialize(X, k):
     Returns:
         pi, m, S or None, None, None
     """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None
+    if not isinstance(k, int) or k < 1:
+        return None, None, None
+    n, d = X.shape
+    phi = np.ones(k)/k
+    m, _ = kmeans(X, k)
+    S = np.tile(np.identity(d), (k, 1)).reshape(k, d, d)
+    return phi, m, S
