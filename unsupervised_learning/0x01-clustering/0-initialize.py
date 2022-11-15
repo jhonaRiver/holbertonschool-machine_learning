206a640f6d566359eebbd6e33cece347aac99906
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-11-15 10:36:52 -0400

    Added file task 0

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -13,3 +13,11 @@ def initialize(X, k):
     Returns:
         ndarray containing the initialized centroids for each cluster or None
     """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(k, int) or k <= 0:
+        return None
+    n, d = X.shape
+    X_min = X.min(axis=0)
+    X_max = X.max(axis=0)
+    return np.random.uniform(X_min, X_max, size=(k, d))
