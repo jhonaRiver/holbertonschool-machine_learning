ccfdd32770b2a2fa6d0651b84b5b8a1b0812bdd5
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-11-16 15:09:42 -0400

    Added file task 11

--- unsupervised_learning/0x01-clustering/11-gmm.py
+++ unsupervised_learning/0x01-clustering/11-gmm.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Module gmm."""
-import numpy as np
 import sklearn.mixture
 
 
@@ -14,3 +13,11 @@ def gmm(X, k):
     Returns:
         pi, m, S, clss, bic
     """
+    g = sklearn.mixture.GaussianMixture(n_components=k)
+    g.fit(X)
+    pi = g.weights_
+    m = g.means_
+    S = g.covariances_
+    clss = g.predict(X)
+    bic = g.bic(X)
+    return pi, m, S, clss, bic
