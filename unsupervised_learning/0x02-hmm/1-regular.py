86e85390a0af077745d5e3ddcd654fb1e0cad088
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-11-22 16:24:02 -0400

    Added file task 1

--- unsupervised_learning/0x02-hmm/1-regular.py
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -12,3 +12,17 @@ def regular(P):
     Returns:
         ndarray containing the steady state probabilities or None
     """
+    try:
+        if len(P.shape) != 2:
+            return None
+        n = P.shape[0]
+        if n != P.shape[1]:
+            return None
+        evals, evecs = np.linalg.eig(P.T)
+        state = (evecs / evecs.sum())
+        new_state = np.dot(state.T, P)
+        for i in new_state:
+            if (i >= 0).all() and np.isclose(i.sum(), 1):
+                return i.reshape(1, n)
+    except Exception:
+        return None
