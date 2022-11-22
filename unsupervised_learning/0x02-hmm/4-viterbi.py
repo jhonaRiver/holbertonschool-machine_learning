e9d6ff32c97f875da464e0904863128e25d98846
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-11-22 17:00:30 -0400

    Added file task 4

--- unsupervised_learning/0x02-hmm/4-viterbi.py
+++ unsupervised_learning/0x02-hmm/4-viterbi.py
@@ -20,3 +20,29 @@ def viterbi(Observation, Emission, Transition, Initial):
         P: probability of obtaining the path sequence
         None: failure
     """
+    try:
+        T = Observation.shape[0]
+        N, M = Emission.shape
+        backpointer = np.zeros((N, T))
+        F = np.zeros((N, T))
+        F[:, 0] = Initial.T * Emission[:, Observation[0]]
+        for t in range(1, T):
+            for n in range(N):
+                Transitions = Transition[:, n]
+                Emissions = Emission[n, Observation[t]]
+                F[n, t] = np.amax(Transitions * F[:, t - 1] * Emissions)
+                backpointer[n, t - 1] = np.argmax(Transitions * F[:, t - 1] *
+                                                  Emissions)
+        path = [0 for i in range(T)]
+        last_state = np.argmax(F[:, T - 1])
+        path[0] = last_state
+        backtrack_index = 1
+        for i in range(T - 2, -1, -1):
+            path[backtrack_index] = int(backpointer[int(last_state), i])
+            last_state = backpointer[int(last_state), i]
+            backtrack_index += 1
+        path.reverse()
+        P = np.amax(F[:, T - 1], axis=0)
+        return path, P
+    except Exception:
+        None, None
