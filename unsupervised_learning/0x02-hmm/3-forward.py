2ce49fa6a5a053f057c81e45eb32668673962527
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-11-22 16:45:03 -0400

    Added file task 3

--- unsupervised_learning/0x02-hmm/3-forward.py
+++ unsupervised_learning/0x02-hmm/3-forward.py
@@ -19,3 +19,17 @@ def forward(Observation, Emission, Transition, Initial):
         F: contains the forward path probabilities
         None: failure
     """
+    try:
+        N = Transition.shape[0]
+        T = Observation.shape[0]
+        F = np.zeros((N, T))
+        F[:, 0] = Initial.T * Emission[:, Observation[0]]
+        for t in range(1, T):
+            for n in range(N):
+                Transitions = Transition[:, n]
+                Emissions = Emission[n, Observation[t]]
+                F[n, t] = np.sum(Transitions * F[:, t - 1] * Emissions)
+        P = np.sum(F[:, -1])
+        return P, F
+    except Exception:
+        None, None
