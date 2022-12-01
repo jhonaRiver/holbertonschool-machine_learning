03ccb1ebb687f94e4827c62c011447ae045305b3
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-12-01 09:59:34 -0400

    Added initial files

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/1-gp.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Class GaussianProcess."""
+import numpy as np
+
+
+class GaussianProcess:
+    """Represent a noiseless 1D Gaussian process."""
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """
+        Class constructor.
+
+        Args:
+            X_init (ndarray): represents the inputs already sampled with the
+                              black-box function
+            Y_init (ndarray): represents the outputs of the black-box function
+                              for each input in X_init
+            l (int, optional): length parameter for the kernel. Defaults to 1.
+            sigma_f (int, optional): standard deviation given to the output of
+                                     the black-box function. Defaults to 1.
+        """
+
+    def kernel(self, X1, X2):
+        """
+        Calculate the covariance kernel matrix between two matrices.
+
+        Args:
+            X1 (ndarray): matrix 1
+            X2 (ndarray): matrix 2
+        Returns:
+            convariance kernel matrix
+        """
+
+    def predict(self, X_s):
+        """
+        Predict the mean and standard deviation of points in a Gaussian\
+            process.
+
+        Args:
+            X_s (ndarray): contains all of the points whose mean and standard
+                           deviation should be calculated
+        Returns:
+            mu: contains the mean for each point in X_s
+            sigma: contains the variance for each point in X_s
+        """
---
+++ unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Class BayesianOptimization."""
+import numpy as np
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization:
+    """Perform Bayesian optimization on a noiseless 1D Gaussian process."""
+
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1,
+                 xsi=0.01, minimize=True):
+        """
+        Class constructor.
+
+        Args:
+            f (function): black-box function to be optimized
+            X_init (ndarray): represents the inputs already sampled with the
+                              black-box function
+            Y_init (ndarray): represents the outputs of the black-box function
+                              for each input in X_init
+            bounds (tuple): represents the bounds of the space in which to
+                            look for the optimal point
+            ac_samples (int): number of samples that should be analyzed during
+                              acquisition
+            l (int, optional): length parameter for the kernel. Defaults to 1.
+            sigma_f (int, optional): standard deviation given to the output of
+                                     the black-box function. Defaults to 1.
+            xsi (float, optional): exploration-exploitation factor for
+                                   acquisition. Defaults to 0.01.
+            minimize (bool, optional): determines whether optimization should
+                                       be performed for minimization or
+                                       maximization. Defaults to True.
+        """
