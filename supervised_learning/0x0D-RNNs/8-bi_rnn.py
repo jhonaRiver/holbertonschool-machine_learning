48aae90c5f207490d90c3041cb20c206f5b77f96
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-12-15 17:14:55 -0400

    Added file task 8

--- supervised_learning/0x0D-RNNs/8-bi_rnn.py
+++ supervised_learning/0x0D-RNNs/8-bi_rnn.py
@@ -16,3 +16,17 @@ def bi_rnn(bi_cell, X, h_0, h_t):
         H: contains all of the concatenated hidden states
         Y: contains all of the outputs
     """
+    t, m, i = X.shape
+    time_step = range(t)
+    _, h = h_0.shape
+    H_f = np.zeros((t+1, m, h))
+    H_b = np.zeros((t+1, m, h))
+    H_f[0] = h_0
+    H_b[t] = h_t
+    for ts in time_step:
+        H_f[ts+1] = bi_cell.forward(H_f[ts], X[ts])
+    for ri in range(t-1, -1, -1):
+        H_b[ri] = bi_cell.backward(H_b[ri+1], X[ri])
+    H = np.concatenate((H_f[1:], H_b[:t]), axis=-1)
+    Y = bi_cell.output(H)
+    return H, Y
