1bef684f1db0e2c4da47528458a5084b862b28cf
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-12-15 14:40:25 -0400

    Added file task 1

--- supervised_learning/0x0D-RNNs/1-rnn.py
+++ supervised_learning/0x0D-RNNs/1-rnn.py
@@ -15,3 +15,15 @@ def rnn(rnn_cell, X, h_0):
         H: contains all of the hidden states
         Y: contains all of the outputs
     """
+    Y = []
+    t, m, i = X.shape
+    time_step = range(t)
+    _, h = h_0.shape
+    H = np.zeros((t+1, m, h))
+    H[0, :, :] = h_0
+    for ts in time_step:
+        h_next, y_pred = rnn_cell.forward(H[ts], X[ts])
+        H[ts+1, :, :] = h_next
+        Y.append(y_pred)
+    Y = np.array(Y)
+    return H, Y
