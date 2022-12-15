15b17fc039aa400fec57a923b84311f6e836460f
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-12-15 15:56:52 -0400

    Added file task 4

--- supervised_learning/0x0D-RNNs/4-deep_rnn.py
+++ supervised_learning/0x0D-RNNs/4-deep_rnn.py
@@ -16,3 +16,20 @@ def deep_rnn(rnn_cells, X, h_0):
         H: contains all of the hidden states
         Y: contains all of the outputs
     """
+    Y = []
+    t, m, i = X.shape
+    _, _, h = h_0.shape
+    time_step = range(t)
+    layers = len(rnn_cells)
+    H = np.zeros((t+1, layers, m, h))
+    H[0, :, :, :] = h_0
+    for ts in time_step:
+        for ly in range(layers):
+            if ly == 0:
+                h_next, y_pred = rnn_cells[ly].forward(H[ts, ly], X[ts])
+            else:
+                h_next, y_pred = rnn_cells[ly].forward(H[ts, ly], h_next)
+            H[ts+1, ly, :, :] = h_next
+        Y.append(y_pred)
+    Y = np.array(Y)
+    return H, Y
