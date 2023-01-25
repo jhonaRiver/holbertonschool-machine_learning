f3d46d2d107f4716c3aeed937cc9aaeadafe0177
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-01-25 19:44:18 -0400

    Added file task 0

--- supervised_learning/0x12-transformer_apps/0-dataset.py
+++ supervised_learning/0x12-transformer_apps/0-dataset.py
@@ -9,6 +9,12 @@ class Dataset:
 
     def __init__(self):
         """Class constructor."""
+        examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
+                                       with_info=True, as_supervised=True)
+        self.data_train, self.data_valid = examples['train'], \
+            examples['validation']
+        self.tokenizer_pt = self.tokenizer_en = self.tokenize_dataset(
+            self.data_train)
 
     def tokenize_dataset(self, data):
         """
@@ -20,3 +26,9 @@ class Dataset:
             tokenizer_pt: Portuguese tokenizer
             tokenizer_en: English tokenizer
         """
+        SubwordTextEncoder = tfds.deprecated.text.SubwordTextEncoder
+        tokenizer_pt = SubwordTextEncoder.build_from_corpus(
+            (pt.numpy() for pt, en in data), target_vocab_size=(2 ** 15))
+        tokenizer_en = SubwordTextEncoder.build_from_corpus(
+            (en.numpy() for pt, en in data), target_vocab_size=(2 ** 15))
+        return tokenizer_pt, tokenizer_en
