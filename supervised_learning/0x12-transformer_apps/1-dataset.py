c33b2538bf85ea262697ce9bf405f5ae1e1cd1c8
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-01-25 20:24:34 -0400

    Added file task 1

--- supervised_learning/0x12-transformer_apps/1-dataset.py
+++ supervised_learning/0x12-transformer_apps/1-dataset.py
@@ -9,6 +9,12 @@ class Dataset:
 
     def __init__(self):
         """Class constructor."""
+        examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
+                                       with_info=True, as_supervised=True)
+        self.data_train, self.data_valid = examples['train'], \
+            examples['validation']
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train)
 
     def tokenize_dataset(self, data):
         """
@@ -20,6 +26,12 @@ class Dataset:
             tokenizer_pt: Portuguese tokenizer
             tokenizer_en: English tokenizer
         """
+        SubwordTextEncoder = tfds.deprecated.text.SubwordTextEncoder
+        tokenizer_pt = SubwordTextEncoder.build_from_corpus(
+            (pt.numpy() for pt, en in data), target_vocab_size=(2 ** 15))
+        tokenizer_en = SubwordTextEncoder.build_from_corpus(
+            (en.numpy() for pt, en in data), target_vocab_size=(2 ** 15))
+        return tokenizer_pt, tokenizer_en
 
     def encode(self, pt, en):
         """
@@ -32,3 +44,12 @@ class Dataset:
             pt_tokens: contains the Portuguese tokens
             en_tokens: contains the English tokens
         """
+        pt_start_index = self.tokenizer_pt.vocab_size
+        pt_end_index = pt_start_index + 1
+        en_start_index = self.tokenizer_en.vocab_size
+        en_end_index = en_start_index + 1
+        pt_tokens = [pt_start_index] + \
+            self.tokenizer_pt.encode(pt.numpy()) + [pt_end_index]
+        en_tokens = [en_start_index] + \
+            self.tokenizer_en.encode(en.numpy()) + [en_end_index]
+        return pt_tokens, en_tokens
