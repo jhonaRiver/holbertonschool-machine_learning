eb2281a425d2df012ad3835f194049d09c7c229c
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-01-26 20:14:13 -0400

    Added file task 2

--- supervised_learning/0x12-transformer_apps/2-dataset.py
+++ supervised_learning/0x12-transformer_apps/2-dataset.py
@@ -9,6 +9,16 @@ class Dataset:
 
     def __init__(self):
         """Class constructor."""
+        data_train = tfds.load("ted_hrlr_translate/pt_to_en",
+                               split="train",
+                               as_supervised=True)
+        data_valid = tfds.load("ted_hrlr_translate/pt_to_en",
+                               split="validation",
+                               as_supervised=True)
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            data_train)
+        self.data_train = data_train.map(self.tf_encode)
+        self.data_valid = data_valid.map(self.tf_encode)
 
     def tokenize_dataset(self, data):
         """
@@ -20,6 +30,14 @@ class Dataset:
             tokenizer_pt: Portuguese tokenizer
             tokenizer_en: English tokenizer
         """
+        SubwordTextEncoder = tfds.deprecated.text.SubwordTextEncoder
+        tokenizer_pt = SubwordTextEncoder.build_from_corpus(
+            (pt.numpy() for pt, en in data),
+            target_vocab_size=(2 ** 15))
+        tokenizer_en = SubwordTextEncoder.build_from_corpus(
+            (en.numpy() for pt, en in data),
+            target_vocab_size=(2 ** 15))
+        return tokenizer_pt, tokenizer_en
 
     def encode(self, pt, en):
         """
@@ -32,6 +50,15 @@ class Dataset:
             pt_tokens: contains the Portuguese tokens
             en_tokens: contains the English tokens
         """
+        pt_start_index = self.tokenizer_pt.vocab_size
+        pt_end_index = pt_start_index + 1
+        en_start_index = self.tokenizer_en.vocab_size
+        en_end_index = en_start_index + 1
+        pt_tokens = [pt_start_index] + self.tokenizer_pt.encode(
+            pt.numpy()) + [pt_end_index]
+        en_tokens = [en_start_index] + self.tokenizer_en.encode(
+            en.numpy()) + [en_end_index]
+        return pt_tokens, en_tokens
 
     def tf_encode(self, pt, en):
         """
@@ -39,5 +66,10 @@ class Dataset:
 
         Args:
             pt (Tensor): contains the Portuguese sentence
-            en (Tensor): contains the corresponding English sentence
         """
+        pt_encoded, en_encoded = tf.py_function(func=self.encode,
+                                                inp=[pt, en],
+                                                Tout=[tf.int64, tf.int64])
+        pt_encoded.set_shape([None])
+        en_encoded.set_shape([None])
+        return pt_encoded, en_encoded
