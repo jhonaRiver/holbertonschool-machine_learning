070ea366be8c87ec39ff11d8f1929c2dce575087
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-01-26 20:31:41 -0400

    Added file task 4

--- supervised_learning/0x12-transformer_apps/4-create_masks.py
+++ supervised_learning/0x12-transformer_apps/4-create_masks.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Module create_masks."""
 import tensorflow.compat.v2 as tf
-import tensorflow_datasets as tfds
 
 
 def create_masks(inputs, target):
@@ -16,3 +15,15 @@ def create_masks(inputs, target):
         combined_mask: padding mask used in the 1st attention block
         decoder_mask: padding mask used in the 2nd attention block
     """
+    encoder_mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)
+    encoder_mask = encoder_mask[:, tf.newaxis, tf.newaxis, :]
+    decoder_mask = tf.cast(tf.math.equal(inputs, 0), tf.float32)
+    decoder_mask = decoder_mask[:, tf.newaxis, tf.newaxis, :]
+    batch_size, seq_len_out = target.shape
+    look_ahead_mask = tf.linalg.band_part(
+        tf.ones((seq_len_out, seq_len_out)), -1, 0)
+    look_ahead_mask = 1 - look_ahead_mask
+    padding_mask = tf.cast(tf.math.equal(target, 0), tf.float32)
+    padding_mask = padding_mask[:, tf.newaxis, tf.newaxis, :]
+    combined_mask = tf.maximum(look_ahead_mask, padding_mask)
+    return encoder_mask, combined_mask, decoder_mask
