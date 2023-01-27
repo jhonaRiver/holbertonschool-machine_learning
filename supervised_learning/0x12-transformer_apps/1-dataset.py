469a12f8ab8ae11dd206327aa848b4679211eb72
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-01-26 20:00:45 -0400

    Added file task 1

--- supervised_learning/0x12-transformer_apps/1-dataset.py
+++ supervised_learning/0x12-transformer_apps/1-dataset.py
@@ -9,10 +9,12 @@ class Dataset:
 
     def __init__(self):
         """Class constructor."""
-        examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
-                                       with_info=True, as_supervised=True)
-        self.data_train, self.data_valid = examples['train'], \
-            examples['validation']
+        self.data_train = tfds.load("ted_hrlr_translate/pt_to_en",
+                                    split="train",
+                                    as_supervised=True)
+        self.data_valid = tfds.load("ted_hrlr_translate/pt_to_en",
+                                    split="validation",
+                                    as_supervised=True)
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
             self.data_train)
 
@@ -28,9 +30,11 @@ class Dataset:
         """
         SubwordTextEncoder = tfds.deprecated.text.SubwordTextEncoder
         tokenizer_pt = SubwordTextEncoder.build_from_corpus(
-            (pt.numpy() for pt, en in data), target_vocab_size=(2 ** 15))
+            (pt.numpy() for pt, en in data),
+            target_vocab_size=(2 ** 15))
         tokenizer_en = SubwordTextEncoder.build_from_corpus(
-            (en.numpy() for pt, en in data), target_vocab_size=(2 ** 15))
+            (en.numpy() for pt, en in data),
+            target_vocab_size=(2 ** 15))
         return tokenizer_pt, tokenizer_en
 
     def encode(self, pt, en):
@@ -48,8 +52,8 @@ class Dataset:
         pt_end_index = pt_start_index + 1
         en_start_index = self.tokenizer_en.vocab_size
         en_end_index = en_start_index + 1
-        pt_tokens = [pt_start_index] + \
-            self.tokenizer_pt.encode(pt.numpy()) + [pt_end_index]
-        en_tokens = [en_start_index] + \
-            self.tokenizer_en.encode(en.numpy()) + [en_end_index]
+        pt_tokens = [pt_start_index] + self.tokenizer_pt.encode(
+            pt.numpy()) + [pt_end_index]
+        en_tokens = [en_start_index] + self.tokenizer_en.encode(
+            en.numpy()) + [en_end_index]
         return pt_tokens, en_tokens
