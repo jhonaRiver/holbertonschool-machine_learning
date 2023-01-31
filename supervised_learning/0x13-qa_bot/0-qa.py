28f51ca82998b50a868b5a77a05abf831e5bc115
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-01-30 21:25:04 -0400

    Added file task 0

--- supervised_learning/0x13-qa_bot/0-qa.py
+++ supervised_learning/0x13-qa_bot/0-qa.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 """Module question_answer."""
+import tensorflow as tf
+import tensorflow_hub as th
+from transformers import BertTokenizer
 
 
 def question_answer(question, reference):
@@ -12,3 +15,25 @@ def question_answer(question, reference):
     Returns:
         string containing the answer
     """
+    tokenizer = BertTokenizer.from_pretrained(
+        'bert-large-uncased-whole-word-masking-finetuned-squad')
+    model = th.load("https://tfhub.dev/see--/bert-uncased-tf2-qa/1")
+    quest_tokens = tokenizer.tokenize(question)
+    refer_tokens = tokenizer.tokenize(reference)
+    tokens = ['[CLS]'] + quest_tokens + ['[SEP]'] + refer_tokens + ['[SEP]']
+    input_word_ids = tokenizer.convert_tokens_to_ids(tokens)
+    input_mask = [1] * len(input_word_ids)
+    input_type_ids = [0] * (1 + len(quest_tokens) + 1) + \
+        [1] * (len(refer_tokens) + 1)
+    input_word_ids, input_mask, input_type_ids = map(lambda t: tf.expand_dims(
+        tf.convert_to_tensor(t, dtype=tf.int32), 0), (input_word_ids,
+                                                      input_mask,
+                                                      input_type_ids))
+    outputs = model([input_word_ids, input_mask, input_type_ids])
+    short_start = tf.argmax(outputs[0][0][1:]) + 1
+    short_end = tf.argmax(outputs[1][0][1:]) + 1
+    answer_tokens = tokens[short_start: short_end + 1]
+    answer = tokenizer.convert_tokens_to_string(answer_tokens)
+    if answer is None or answer is "" or question in answer:
+        return None
+    return answer
