f8c0f3bf735436a2bd29fe62c7c14664420bb292
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-01-30 20:58:19 -0400

    Added file task 1

--- supervised_learning/0x13-qa_bot/1-loop.py
+++ supervised_learning/0x13-qa_bot/1-loop.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 """Script that takes in input from the user and responds."""
+
+
+words = ['bye', 'goodbye', 'quit', 'exit']
+while True:
+    request = input("Q: ")
+    if request.lower() in words:
+        print('A: Goodbye')
+        break
+    else:
+        print('A: ')
