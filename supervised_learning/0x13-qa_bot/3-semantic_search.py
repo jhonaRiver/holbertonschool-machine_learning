c9842cabc107d2961b0ce7ae1d082ff23a6f3ab4
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-02-14 20:57:05 -0400

    Added file task 3

--- supervised_learning/0x13-qa_bot/3-semantic_search.py
+++ supervised_learning/0x13-qa_bot/3-semantic_search.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 """Module semantic_search."""
+import numpy as np
+import os
+import tensorflow_hub as hub
 
 
 def semantic_search(corpus_path, sentence):
@@ -13,3 +16,16 @@ def semantic_search(corpus_path, sentence):
     Returns:
         reference text of the document most similar to sentence
     """
+    documents = [sentence]
+    for filename in os.listdir(corpus_path):
+        if filename.endswith('.md') is False:
+            continue
+        with open(corpus_path + '/' + filename, 'r', encoding='utf-8') as f:
+            documents.append(f.read())
+    model = hub.load(
+        "https://tfhub.dev/google/universal-sentence-encoder-large/5")
+    embeddings = model(documents)
+    correlation = np.inner(embeddings, embeddings)
+    closest = np.argmax(correlation[0, 1:])
+    similar = documents[closest + 1]
+    return similar
