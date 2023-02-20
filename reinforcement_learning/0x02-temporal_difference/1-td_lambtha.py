57e88e35b3458f58b4508f884b2daee417ae6960
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-02-20 17:47:39 -0400

    Added file task 1

--- reinforcement_learning/0x02-temporal_difference/1-td_lambtha.py
+++ reinforcement_learning/0x02-temporal_difference/1-td_lambtha.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Module td_lambtha."""
+import gym
+import numpy as np
 
 
 def td_lambtha(env, V, policy, lambtha, episodes=5000, max_steps=100,
@@ -21,3 +23,37 @@ def td_lambtha(env, V, policy, lambtha, episodes=5000, max_steps=100,
     Returns:
         V: updated value estimate
     """
+    # Loop for the given number of episodes
+    for i in range(episodes):
+        # Initialize eligibility trace and state
+        E = np.zeros(V.shape)
+        state = env.reset()
+
+        # Loop for the given number of steps
+        for j in range(max_steps):
+            # Get the action to take from the policy
+            action = policy(state)
+
+            # Take the action and observe the next state, reward, and done flag
+            next_state, reward, done, _ = env.step(action)
+
+            # Calculate the TD error
+            delta = reward + gamma * V[next_state] - V[state]
+
+            # Update the eligibility trace
+            E[state] += 1
+
+            # Loop over all states and update the value estimate and
+            # eligibility trace
+            for s in range(env.observation_space.n):
+                V[s] += alpha * delta * E[s]
+                E[s] *= gamma * lambtha
+
+            # Update the state
+            state = next_state
+
+            # If the episode is done, break out of the loop
+            if done:
+                break
+
+    return V
