45424834aea9ef90b4c21c57212ed1f00994588e
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-02-20 17:47:31 -0400

    Added file task 0

--- reinforcement_learning/0x02-temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/0x02-temporal_difference/0-monte_carlo.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Module monte_carlo."""
+# import gym
+# import numpy as np
 
 
 def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1,
@@ -20,3 +22,38 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1,
     Returns:
         V: updated value estimate
     """
+    # Loop for the given number of episodes
+    for i in range(episodes):
+        # Initialize episode list and state
+        episode = []
+        state = env.reset()
+
+        # Loop for the given number of steps
+        for j in range(max_steps):
+            # Get the action to take from the policy
+            action = policy(state)
+
+            # Take the action and observe the next state, reward, and done flag
+            next_state, reward, done, _ = env.step(action)
+
+            # Add the (state, action, reward) tuple to the episode
+            episode.append((state, action, reward))
+
+            # Update the state
+            state = next_state
+
+            # If the episode is done, break out of the loop
+            if done:
+                break
+
+        # Loop over the episode in reverse order to calculate the returns and
+        # update the value estimate
+        G = 0
+        for state, action, reward in reversed(episode):
+            # Calculate the return
+            G = gamma * G + reward
+
+            # Update the value estimate
+            V[state] += alpha * (G - V[state])
+
+    return V
