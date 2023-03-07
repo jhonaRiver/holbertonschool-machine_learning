ab2ce788f511bd4de9576a1f46cc3cd222adfa0e
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-03-07 19:29:30 -0400

    Added file task 1

--- pipeline/0x00-pandas/1-from_dictionary.py
+++ pipeline/0x00-pandas/1-from_dictionary.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 """Script that creates a dataframe from a dictionary."""
 import pandas as pd
+
+
+# Define the dictionary with the data
+data_dict = {'First': [0.0, 0.5, 1.0, 1.5],
+             'Second': ['one', 'two', 'three', 'four']}
+
+# Create the DataFrame with the specified row and column labels
+df = pd.DataFrame(data_dict, index=['A', 'B', 'C', 'D'])
