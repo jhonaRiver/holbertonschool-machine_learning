ac4002c1ee1852f50d09d0111f833740d0c206ee
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-03-07 19:52:48 -0400

    Added file task 5

--- pipeline/0x00-pandas/5-slice.py
+++ pipeline/0x00-pandas/5-slice.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+"""Slice script."""
 
 import pandas as pd
 from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-df = # YOUR CODE HERE
+df = df[['High', 'Low', 'Close', 'Volume_(BTC)']].iloc[::60]
 
 print(df.tail())
