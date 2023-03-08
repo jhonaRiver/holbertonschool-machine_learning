417438322c96ba5abe1d26a4d1a0379b5d4fdd8a
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-03-07 20:07:05 -0400

    Added file task 9

--- pipeline/0x00-pandas/9-fill.py
+++ pipeline/0x00-pandas/9-fill.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
+"""Fill script."""
 
 import pandas as pd
 from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-# YOUR CODE HERE
+# Remove the Weighted_Price column
+df = df.drop('Weighted_Price', axis=1)
+
+# Fill in the missing data points
+df['Close'].fillna(method='ffill', inplace=True)
+df['High'].fillna(df['Close'], inplace=True)
+df['Low'].fillna(df['Close'], inplace=True)
+df['Open'].fillna(df['Close'], inplace=True)
+df['Volume_(BTC)'].fillna(0, inplace=True)
+df['Volume_(Currency)'].fillna(0, inplace=True)
 
 print(df.head())
 print(df.tail())
