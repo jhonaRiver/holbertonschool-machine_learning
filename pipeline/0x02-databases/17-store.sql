96667c6236192d11e67a25b6aa6909cb9cbe0c1a
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-03-18 17:34:58 -0400

    Added file task 17

--- pipeline/0x02-databases/17-store.sql
+++ pipeline/0x02-databases/17-store.sql
@@ -1,9 +1,8 @@
 -- Creates a trigger that decreases the quantity of an item after adding a new order
-DROP TRIGGER IF EXISTS decrease_quantity;
-DELIMITER $$ CREATE TRIGGER decrease_quantity
+CREATE TRIGGER decrease_quantity
 AFTER
-INSERT ON `orders` FOR EACH ROW BEGIN
+INSERT ON orders FOR EACH ROW BEGIN
 UPDATE items
-SET quantity = quantity - new.number
-WHERE items.name = new.item_name;
-END $$ DELIMITER;
+SET quantity = quantity - NEW.quantity
+WHERE item_id = NEW.item_id;
+END;
