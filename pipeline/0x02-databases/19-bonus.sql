284c544a9fd62e64b08c0be51fd33f55a767e505
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-03-18 18:54:31 -0400

    Modified file task 19

--- pipeline/0x02-databases/19-bonus.sql
+++ pipeline/0x02-databases/19-bonus.sql
@@ -1,25 +1,24 @@
--- Creates a stored procedure AddBonus that adds a new connection for a student
-DELIMITER / / CREATE PROCEDURE AddBonus (
-    IN user_id INTEGER,
-    IN project_name VARCHAR(255),
-    IN score INTEGER
-) BEGIN IF NOT EXISTS (
-    SELECT name
+-- Creates a stored procedure AddBonus that adds a new correction for a student
+CREATE PROCEDURE AddBonus (
+    @user_id INT,
+    @project_name NVARCHAR(50),
+    @score INT
+) AS BEGIN -- Check if the project already exists
+IF NOT EXISTS(
+    SELECT 1
     FROM projects
-    WHERE name = project_name
-) THEN
+    WHERE name = @project_name
+) BEGIN -- If the project doesn't exist, create a new project
 INSERT INTO projects(name)
-VALUES (project_name);
-END IF;
-INSERT INTO corrections(user_id, project_id, score)
-VALUES(
-        user_id,
-        (
-            SELECT id
-            FROM projects
-            WHERE name = project_name
-        ),
-        score
+VALUES (@project_name);
+END -- Get the project_id for the given project_name
+DECLARE @project_id INT;
+SET @project_id = (
+        SELECT id
+        FROM projects
+        WHERE name = @project_name
     );
-END;
-/ / DELIMITER;
+-- Insert the correction for the given user and project
+INSERT INTO corrections(user_id, project_id, score)
+VALUES (@user_id, @project_id, @score);
+END
