b0b2caf2701ef16b60a8f4f3663a9b39f748fdf8
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-03-18 19:32:44 -0400

    Modified file task 21

--- pipeline/0x02-databases/21-div.sql
+++ pipeline/0x02-databases/21-div.sql
@@ -1,5 +1,4 @@
 -- Creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0
-DELIMITER / / CREATE FUNCTION SafeDiv (a INT, b INT) RETURNS FLOAT DETERMINISTIC BEGIN IF b = 0 THEN RETURN 0;
-ELSE RETURN (a / b);
-END IF;
-END / / DELIMITER;
+CREATE FUNCTION SafeDiv (@a INT, @b INT) RETURNS FLOAT AS BEGIN IF @b = 0 RETURN 0.0;
+ELSE RETURN CONVERT(FLOAT, @a) / CONVERT(FLOAT, @b);
+END
