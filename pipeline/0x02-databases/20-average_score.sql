517c76844f5b8e5ca31a99e8edc03b82f45d6351
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-03-18 19:21:24 -0400

    Modified file task 20

--- pipeline/0x02-databases/20-average_score.sql
+++ pipeline/0x02-databases/20-average_score.sql
@@ -1,11 +1,13 @@
 -- Creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
-DELIMITER / / CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id_new INTEGER) BEGIN
-UPDATE users
-SET average_score =(
+CREATE PROCEDURE ComputeAverageScoreForUser (@user_id INT) AS BEGIN -- Compute the average score for the given user
+DECLARE @avg_score FLOAT;
+SET @avg_score = (
         SELECT AVG(score)
         FROM corrections
-        WHERE user_id = user_id_new
-    )
-WHERE id = user_id_new;
-END;
-/ / DELIMITER;
+        WHERE user_id = @user_id
+    );
+-- Update the user's average_score in the users table
+UPDATE users
+SET average_score = @avg_score
+WHERE id = @user_id;
+END
