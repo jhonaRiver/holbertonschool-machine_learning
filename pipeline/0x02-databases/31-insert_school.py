3dfd4340c03b935a2750fe9f9af9c5cfd0d97ede
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2023-03-18 20:31:16 -0400

    Added file task 31

---
+++ pipeline/0x02-databases/31-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module insert_school."""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection based on kwargs.
+
+    Args:
+        mongo_collection (obj): pymongo collection object
+    """
+    document = mongo_collection.insert_one(kwargs)
+    return (document.inserted_id)
